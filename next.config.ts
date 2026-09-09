218fae03e501661cc8b63c499d1dc4ef25ae6b1a
Goubadesire2 <goubadesire0@gmail.com>
2026-09-09 22:52:31 +0000

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 import withPWA from 'next-pwa'
 
 const nextConfig: NextConfig = {
-  // Vos configurations Next.js globales ici si besoin
+  turbopack: {},
 }
 
 const pwaConfig = withPWA({
